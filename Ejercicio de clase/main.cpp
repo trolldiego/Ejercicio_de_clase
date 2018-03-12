24f0f0455a85e0536a8cbc2074f5009b06ee339a
Unknown <trolldiego@hotmail.es>
2018-03-11 20:34:51 -0500

    New linw

--- Ejercicio de clase/main.cpp
+++ Ejercicio de clase/main.cpp
@@ -1,9 +1,43 @@
-#include <iostream>
+#include <iostream>// biblioteca entrada y salida datos
 
-using namespace std;
+using namespace std; //palabras reservadas
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int mes = 0 , anio = 0, dato = 0,total=0; //declarar variables
+
+    cout << " Ingrese la fecha" << endl ;
+    cin >> dato;
+
+    if (dato<=31129999)
+    {
+        total=dato/1000000;
+        mes=(dato/10000)%100;
+        anio=(dato%10000);
+
+        if (mes>12)
+        {
+            cout << "Mes erroneo";
+        }
+
+        else if (total>31)
+        {
+            cout << "Dia erroneo";
+        }
+        else
+        {
+            cout << "Es " << total <<" del mes " << mes << " del año " <<anio << endl;
+
+        }
+    }
+    else
+    {
+        cout << "Error";
+    }
+
+
+
+
+
     return 0;
 }
